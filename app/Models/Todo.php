ab0a360504284eec9b38acda23b0469d3cb9ca2a
S Sarfaraz Ali <itsmrsarfaraz@gmail.com>
2026-04-16 11:02:58 +0500

    Add Todo model, migration, controller, and routes

---
+++ app/Models/Todo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'completed_at',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'completed_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
